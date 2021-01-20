90a8b2296fb0a95fee698d1a29ddb3c103e29343
Ghazi Yusaf <ghaziyusaf@gmail.com>
2021-01-20 18:15:26 +0000

    Create homepage
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

---
+++ lib/src/Feed.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pixplace/pages.dart';
+import 'package:pixplace/widgets/PostCardWidget.dart';
+
+class Feed extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+              children: <Widget>[
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+                PostCardWidget(),
+              ]
+          )
+      ),
+    );
+  }
+}
