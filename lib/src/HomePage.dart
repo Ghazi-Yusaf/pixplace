37645fdbe7ecbca00b30aae2d65bf4d9fd529057
aaronmolesbury <aaronmolesbury@gmail.com>
2021-01-20 14:47:43 +0000

    creates cards

--- lib/src/HomePage.dart
+++ lib/src/HomePage.dart
@@ -5,8 +5,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:Text("PixPlace",style:TextStyle(color: Colors.black)), backgroundColor: Colors.white, elevation: 0,),
-      body: Center(
-          child:Text("Home page",textScaleFactor: 2,)
+      body: MyStatelessWidget(),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('<UserName>'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('<Like>'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('<Comment>'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
