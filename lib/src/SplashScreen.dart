29be480cba096ba0534c99898b41a0536cb30e58
Ghazi Yusaf <ghaziyusaf@gmail.com>
2021-01-20 18:39:16 +0000

    Update SplashScreen.dart
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

--- lib/src/SplashScreen.dart
+++ lib/src/SplashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pixplace/pages.dart';
+import 'package:pixplace/src/HomeState.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -15,7 +16,7 @@ class _SplashScreen extends State<SplashScreen> {
             ()=>Navigator.pushReplacement(context,
             MaterialPageRoute(builder:
                 (context) =>
-                LoginPage()
+                Home()
             )
         )    );
   }
