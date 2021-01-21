da5e3cf6df2639fbe0efc5f680d9c0d54f799d49
xecarlox94 <jf94.uk@gmail.com>
2021-01-21 19:19:53 +0000

    working on profile page

--- lib/src/ProfilePage.dart
+++ lib/src/ProfilePage.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
+Widget _buildStack() => Stack(
+  alignment: const Alignment(0, 0),
+  children: [
+    CircleAvatar(
+      backgroundImage: AssetImage('assets/icons/AppIcon.jpg'),
+      radius: 100,
+    ),
+    Container(
+      decoration: BoxDecoration(
+        color: Colors.black45,
+      ),
+    ),
+  ],
+);
+
+
 class ProfilePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
       return Container(
-        color: Colors.black,
+        color: Colors.white60,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildStack(),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'Roboto',
+                letterSpacing: 0.5,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        )
       );
   }
 }
