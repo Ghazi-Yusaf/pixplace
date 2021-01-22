72a85b989cd391cfa4f91cc1d038437262179f6f
aaronmolesbury <aaronmolesbury@gmail.com>
2021-01-22 21:30:04 +0000

    Creates Title

--- lib/src/ChallengePage.dart
+++ lib/src/ChallengePage.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 class ChallengePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
     return Scaffold(
+      appBar: AppBar(
+        title: Text("test")
+      ),
+
+      body: Container(
+        child: Text(
+          "Challenges",
+          textAlign: TextAlign.center,
+          textScaleFactor: 3,
+        ),
+        color: Colors.grey,
+        padding: EdgeInsets.symmetric(vertical: 40),
+        width: _width,
+      ),
+
+
 
     );
   }
