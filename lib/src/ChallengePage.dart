39ba1456d07070262cc5ffd002fd11874615e17f
aaronmolesbury <aaronmolesbury@gmail.com>
2021-01-22 22:38:46 +0000

    fixes out of bounds for tab widget

--- lib/src/ChallengePage.dart
+++ lib/src/ChallengePage.dart
@@ -1,68 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:pixplace/widgets/TabWidget.dart';
+
 class ChallengePage extends StatefulWidget{
   @override
   ChallengePageState createState() => ChallengePageState();
 }
 
+List<Tab> tabs = [
+  Tab(
+    text: "Daily",
+  ),
+  Tab(
+    text: "Weekly",
+  ),
+  Tab(
+    text: "Monthly",
+  ),
+];
+
+List<Widget> contents = [
+  // first tab bar view widget
+  Container(
+    color: Colors.white,
+    child: Center(
+      child: Text(
+        'Daily',
+      ),
+    ),
+  ),
+
+  // second tab bar view widget
+  Container(
+    color: Colors.white,
+    child: Center(
+      child: Text(
+        'Weekly',
+      ),
+    ),
+  ),
+
+  Container(
+    color: Colors.white,
+    child: Center(
+      child: Text(
+        'Monthly',
+      ),
+    ),
+  ),
+];
+
 class ChallengePageState extends State<ChallengePage> {
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(title: Text("PixPlace"),),
-        body: Column(children: [
-          Container(
-            child: Text(
-              "Challenges",
-              textScaleFactor: 3,
-              textAlign: TextAlign.center,
-            )
-          ),
-        SizedBox(
-          height: 50,
-          child: AppBar(
-            backgroundColor: Colors.black,
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  text: "Daily",
-                ),
-                Tab(
-                  text: "Weekly",
-                ),
-              ],
-            ),
-          ),
-        ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                // first tab bar view widget
-                Container(
-                  color: Colors.white,
-                  child: Center(
-                    child: Text(
-                      'Daily',
-                    ),
-                  ),
-                ),
-
-                // second tab bar view widget
-                Container(
-                  color: Colors.white,
-                  child: Center(
-                    child: Text(
-                      'Weekly',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),],
-        ),
-      )
-    );
+    return TabWidget("Does this work ?", tabs, contents,3);
   }
 }
---
+++ lib/widgets/TabWidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Widget TabWidget (String title, List<Tab> tabs, List<Widget> contents, int length) => DefaultTabController(
+    length: length,
+    child: Scaffold(
+      appBar: AppBar(title: Text("PixPlace"),),
+      body: Column(children: [
+        Container(
+            child: Text(
+              title,
+              textScaleFactor: 3,
+              textAlign: TextAlign.center,
+            )
+        ),
+        SizedBox(
+          height: 50,
+          child: AppBar(
+            backgroundColor: Colors.black,
+            bottom: TabBar(
+              tabs: tabs,
+            ),
+          ),
+        ),
+        Expanded(
+          child: TabBarView(
+            children: contents,
+          ),
+        ),],
+      ),
+    )
+);
