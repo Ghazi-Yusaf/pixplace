8353f87c12af1409b84b9d38b74a8c68c9c74835
aaronmolesbury <aaronmolesbury@gmail.com>
2021-01-22 22:17:25 +0000

    implements the tabs

--- lib/src/ChallengePage.dart
+++ lib/src/ChallengePage.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
+class ChallengePage extends StatefulWidget{
+  @override
+  ChallengePageState createState() => ChallengePageState();
+}
 
-class ChallengePage extends StatelessWidget {
+class ChallengePageState extends State<ChallengePage> {
   @override
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("test")
-      ),
-
-      body: Container(
-        child: Text(
-          "Challenges",
-          textAlign: TextAlign.center,
-          textScaleFactor: 3,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(title: Text("PixPlace"),),
+        body: Column(children: [
+          Container(
+            child: Text(
+              "Challenges",
+              textScaleFactor: 3,
+              textAlign: TextAlign.center,
+            )
+          ),
+        SizedBox(
+          height: 50,
+          child: AppBar(
+            backgroundColor: Colors.black,
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Daily",
+                ),
+                Tab(
+                  text: "Weekly",
+                ),
+              ],
+            ),
+          ),
         ),
-        color: Colors.grey,
-        padding: EdgeInsets.symmetric(vertical: 40),
-        width: _width,
-      ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                // first tab bar view widget
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Daily',
+                    ),
+                  ),
+                ),
 
-
-
+                // second tab bar view widget
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Weekly',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),],
+        ),
+      )
     );
   }
 }
