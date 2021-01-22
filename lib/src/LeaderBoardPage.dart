f69b560eb5a4a784b0e53bbb84a69933e6d178bc
xecarlox94 <jf94.uk@gmail.com>
2021-01-22 21:28:34 +0000

    finished tabr bar

--- lib/src/LeaderBoardPage.dart
+++ lib/src/LeaderBoardPage.dart
@@ -5,11 +5,36 @@ class LeaderBoardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    throw DefaultTabController(
+    // return Container(
+    //   child: Text("dsf"),
+    // );
+    return MaterialApp(
+      home: DefaultTabController(
         length: 3,
-        child: Scaffold(
-          appBar: ,
-        ));
+        child: new Scaffold(
+          appBar: new PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: new Container(
+              height: 50.0,
+              child: new TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car,color: Colors.grey,)),
+                  Tab(icon: Icon(Icons.directions_transit,color: Colors.grey)),
+                  Tab(icon: Icon(Icons.directions_bike,color: Colors.grey)),
+                ],
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 }
