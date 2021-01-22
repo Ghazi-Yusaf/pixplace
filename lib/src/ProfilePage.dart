b6a6f10b80ac57f3371712c172f782285fa4375a
xecarlox94 <jf94.uk@gmail.com>
2021-01-22 19:39:31 +0000

    adding gradient to profile page

--- lib/src/ProfilePage.dart
+++ lib/src/ProfilePage.dart
@@ -30,38 +30,67 @@ Widget headerMenu() => Row(
   ],
 );
 
+Widget profileImage() => Stack(
+  alignment: const Alignment(0.0, 0.0),
+  children: [
+    Container(
+      decoration: new BoxDecoration(
+          gradient: RadialGradient(
+              colors: [Colors.yellow, Colors.white],
+              stops: [0.65, 1.0]
+          )
+      ),
+      child: CircleAvatar(
+        radius: 100,
+        backgroundColor: Colors.transparent,
+      ),
+    ),
+    Container(
+      width: 75,
+      height: 75,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: new AssetImage("assets/images/portrait.jpg"),
+          fit: BoxFit.cover
+        )
+      ),
+    ),
+    CircleAvatar(
+      radius: 75,
+      backgroundColor: Colors.blue,
+    ),
+  ],
+);
+
 
 Widget header() => Column(
   mainAxisAlignment: MainAxisAlignment.start,
   children: [
     Padding(
-      padding: const EdgeInsets.only(top: 40.0),
-      child: CircleAvatar(
-        // backgroundImage: AssetImage('assets/AppIcon.jpg'),
-        radius: 100,
-        backgroundColor: Colors.blue,
-      ),
+        padding: const EdgeInsets.only(top: 30.0),
+        child: profileImage()
     ),
     Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15.0),
-        child: Column(
-          children: [
-            Text(
-              "name",
-              style: TextStyle(
+      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      child: Column(
+        children: [
+          Text(
+            "name",
+            style: TextStyle(
                 fontSize: 30.0,
                 color: Colors.black
-              ),
             ),
-            Text(
-              "level",
-              style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.deepOrange
-              ),
+          ),
+          Text(
+            "level",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.deepOrange
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     ),
     headerMenu()
   ],
@@ -86,7 +115,7 @@ class ProfilePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-      return Container(
+    return Container(
         color: Colors.white,
         child: Column(
           children: [
@@ -94,6 +123,6 @@ class ProfilePage extends StatelessWidget{
             // gridGallery()
           ],
         )
-      );
+    );
   }
 }
