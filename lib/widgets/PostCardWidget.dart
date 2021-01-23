6a20c37782a31dba721727d09f3fddf3f6d28ffe
xecarlox94 <jf94.uk@gmail.com>
2021-01-23 23:35:45 +0000

    finished adding comments section

--- lib/widgets/PostCardWidget.dart
+++ lib/widgets/PostCardWidget.dart
@@ -10,7 +10,7 @@ Widget getImage(BuildContext context) => Image.network("https://picsum.photos/25
 
 Widget postDetails() => Row(
   children: [
-    Text('<UserName>'),
+    TextButton(onPressed: () => {}, child: Text('First Last')),
     Spacer(),
     IconButton(icon: Icon(Icons.expand_more_outlined), onPressed: () => {})
   ],
@@ -26,12 +26,39 @@ Widget actionsBar() => Row(
   ],
 );
 
+class Message {
+  final username;
+  final message;
+  const Message(this.username, this.message);
+}
+
+List<Message> comments = [
+  new Message("Joe", "Really interesting"),
+  new Message("Sarah", "I wish I was there"),
+  new Message("Rebecca", "Thats a great picture")
+];
+
+Widget commentField(String username, String message) => ListTile(
+  leading: Text(username),
+  title: Text(message),
+  trailing: Icon(Icons.favorite_outline),
+);
+
+Widget commentsSection() => ExpansionTile(
+    title: Text("3 People commented"),
+    trailing: Text(""),
+    children: comments.map((comment) => commentField(comment.username, comment.message)).toList()
+);
+
+// Text("Comments Section");
+
 Widget cardContent(BuildContext context) => Container(
   child: Column(
     children: [
       postDetails(),
       getImage(context),
-      actionsBar()
+      actionsBar(),
+      commentsSection(),
     ],
   ),
 );
