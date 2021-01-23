44f7557540b3d00e07371285ee87257788cd8df6
xecarlox94 <jf94.uk@gmail.com>
2021-01-23 21:57:50 +0000

    added image to feed card

--- lib/widgets/PostCardWidget.dart
+++ lib/widgets/PostCardWidget.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
 
+Widget getImage(BuildContext context) => Image.network("https://picsum.photos/250/150",
+    fit: BoxFit.fitWidth,
+    width: MediaQuery.of(context).size.width
+  // loadingBuilder: (context, child, progress) => {
+  //     return progress == null ? child : LinearProgressIndicator()
+  // },
+);
+
+// Widget imageThumb() => Flex(direction: Axis.horizontal,
+//     children: [
+//       Expanded(child: getImage()),
+//     ]
+// );
+
+
+// ListTile(
+//   title: Text('<UserName>'),
+// ),
+// TextButton(
+//   child: const Text('<Like>'),
+//   onPressed: () {/* ... */},
+// ),
+// const SizedBox(width: 8),
+// TextButton(
+//   child: const Text('<Comment>'),
+//   onPressed: () {/* ... */},
+// ),
+// const SizedBox(width: 8)
+
+Widget cardContent(BuildContext context) => Container(
+  child: Column(
+    children: [
+      getImage(context),
+      Text('<Like>'),
+      ListTile(
+        title: Text('<UserName>'),
+      ),
+      TextButton(
+        child: const Text('<Like>'),
+        onPressed: () {/* ... */},
+      ),
+      const SizedBox(width: 8),
+      TextButton(
+        child: const Text('<Comment>'),
+        onPressed: () {/* ... */},
+      )
+    ],
+  ),
+);
+
 class PostCardWidget extends StatelessWidget {
 
 
@@ -7,32 +57,9 @@ class PostCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              title: Text('<UserName>'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('<Like>'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('<Comment>'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
-      ),
+    return Card(
+      child: cardContent(context),
     );
   }
 }
+
