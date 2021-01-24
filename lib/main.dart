5cc4e5d9e23bbeb22e77e0b53868ee168da420ee
xecarlox94 <jf94.uk@gmail.com>
2021-01-24 22:11:24 +0000

    adding camera code and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+
+
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'src/Camera.dart';
+
+// https://pub.dev/packages/camera
+// https://flutter.dev/docs/cookbook/plugins/picture-using-camera
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(CameraApp(cameras));
 }
+
+
+
 
 class MyApp extends StatelessWidget {
   @override
---
+++ lib/src/Camera.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+// https://pub.dev/packages/camera
+// https://flutter.dev/docs/cookbook/plugins/picture-using-camera
+
+class CameraApp extends StatefulWidget {
+  List<CameraDescription> cameras;
+
+  CameraApp(this.cameras);
+
+  @override
+  _CameraAppState createState() => _CameraAppState(this.cameras);
+}
+
+class _CameraAppState extends State<CameraApp> {
+  CameraController controller;
+  List<CameraDescription> cameras;
+
+  _CameraAppState(this.cameras);
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: CameraPreview(controller));
+  }
+}
