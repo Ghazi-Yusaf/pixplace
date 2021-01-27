9db40e4f6e5e50aa8b875658606564e8e048ad9a
xecarlox94 <jf94.uk@gmail.com>
2021-01-27 21:55:42 +0000

    settings page

--- lib/src/SettingsPage.dart
+++ lib/src/SettingsPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:clean_settings/clean_settings.dart';
+import 'package:pixplace/widgets/ButtonWidget.dart';
 
 // docs:
 // https://pub.dev/packages/clean_settings
@@ -10,51 +11,42 @@ String status = "Available";
 bool smartReply = true;
 bool notifications = true;
 
+SettingSection appSection() => SettingSection(title: "App", items: [
+      SettingItem(title: "Privacy Policy", onTap: () => {}),
+      SettingItem(title: "Help", onTap: () => {}),
+    ]);
 
-SettingSection accountSection() {
+SettingSection accountSection() => SettingSection(title: "Account", items: [
+      SettingRadioItem<String>(
+          title: "Status",
+          displayValue: "($status)",
+          selectedValue: status,
+          items: [
+            SettingRadioValue("Busy", "Busy"),
+            SettingRadioValue("Available", "Available"),
+            SettingRadioValue("Offline", "Offline")
+          ],
+          onChanged: (v) => status = v)
+    ]);
 
-  return SettingSection(
-    title: "Account",
+SettingSection generalSection() => SettingSection(
+      title: "General",
       items: [
-        SettingRadioItem<String>(
-            title: "Status",
-            displayValue: "($status)",
-            selectedValue: status,
-            items: [
-              SettingRadioValue("Busy", "Busy"),
-              SettingRadioValue("Available", "Available"),
-              SettingRadioValue("Offline", "Offline")
-            ],
-            onChanged: (v) => status = v
-        )
-      ]
-  );
-}
+        SettingCheckboxItem(
+          title: 'Location',
+          value: smartReply,
+          onChanged: (v) => smartReply = v,
+          description: 'Tracks your position while using the app',
+          priority: ItemPriority.low,
+        ),
+        SettingSwitchItem(
+          title: "Notifications",
+          value: notifications,
+          onChanged: (v) => notifications = v,
+          description: "Notifies you about important updates",
+        ),
+      ],
+    );
 
-
-SettingSection generalSection() {
-  return SettingSection(
-    title: "General",
-    items: [
-      SettingCheckboxItem(
-        title: 'Location',
-        value: smartReply,
-        onChanged: (v) => smartReply = v,
-        description: 'Tracks your position while using the app',
-        priority: ItemPriority.low,
-      ),
-      SettingSwitchItem(
-        title: "Notifications",
-        value: notifications,
-        onChanged: (v) => notifications = v,
-        description: "Notifies you about important updates",
-      ),
-    ],
-  );
-}
-
-
-Widget SettingsPage() => SettingContainer(sections: [
-  generalSection(),
-  accountSection()
-]);
+Widget SettingsPage() => SettingContainer(
+    sections: [generalSection(), accountSection(), appSection()]);
