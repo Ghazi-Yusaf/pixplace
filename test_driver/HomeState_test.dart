0fdf117595214b479fc7c38d9ab9a55955c52365
Tania <taniahenderson1@hotmail.co.uk>
2021-01-31 22:33:53 +0000

    Created test for bottom navbar. Minor changes to src/HomeState.dart for the test files (replaced label with title)

--- test_driver/HomeState_test.dart
+++ test_driver/HomeState_test.dart
@@ -19,23 +19,23 @@ void main() {
 
   test('bottomnavigationbar test', () async {
     await driver.waitFor(find.byValueKey('bottom'));
-    //await driver.tap(find.text('Home'));
+
     final firstItem = find.byValueKey('Home');
     await driver.tap(firstItem);
     print('clicked on Home');
-    //await driver.tap(find.text('Profile'));
+
     final secondItem = find.byValueKey('Profile');
     await driver.tap(secondItem);
     print('clicked on Profile');
-    //await driver.tap(find.text('Post'));
+
     final thirdItem = find.byValueKey('Post');
     await driver.tap(thirdItem);
     print('clicked on Post');
-    //await driver.tap(find.text('Achievements'));
+
     final fourthItem = find.byValueKey('Achievements');
     await driver.tap(fourthItem);
     print('clicked on Achievements');
-    //await driver.tap(find.text('Leaderbaords'));
+
     final fifthItem = find.byValueKey('Leaderboards');
     await driver.tap(fifthItem);
     print('clicked on Leaderbaords');
