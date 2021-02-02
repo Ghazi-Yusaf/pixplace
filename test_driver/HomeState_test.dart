6b4369b56445735409e2d1da7c5fe3aee5d72122
Tania <taniahenderson1@hotmail.co.uk>
2021-02-02 00:39:46 +0000

    Fixed a typo on line 47. Added some comments, including what line to run in the terminal (for convenience)

--- test_driver/HomeState_test.dart
+++ test_driver/HomeState_test.dart
@@ -1,15 +1,18 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
+// run test using "flutter drive --target=test_driver/HomeState.dart" in terminal
 
 void main() {
 
   FlutterDriver driver;
 
+  // initiates connection to the driver
   setUpAll(() async {
     driver = await FlutterDriver.connect();
   });
 
+  // severs connection to the driver after test is complete
   tearDownAll(() async {
     if (driver != null) {
       driver.close();
@@ -18,8 +21,11 @@ void main() {
 
 
   test('bottomnavigationbar test', () async {
+
+    // locates the section of code for testing using Keys
     await driver.waitFor(find.byValueKey('bottom'));
 
+    // driver taps on the button with the specified Key and prints the message if successful
     final firstItem = find.byValueKey('Home');
     await driver.tap(firstItem);
     print('clicked on Home');
@@ -38,6 +44,6 @@ void main() {
 
     final fifthItem = find.byValueKey('Leaderboards');
     await driver.tap(fifthItem);
-    print('clicked on Leaderbaords');
+    print('clicked on Leaderboards');
   });
 }
