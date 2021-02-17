1dab5acd5c4dff762f0b328f5b192e757de48e23
xecarlox94 <jf94.uk@gmail.com>
2021-02-17 23:22:58 +0000

    adding search import to main file

--- lib/src/HomeState.dart
+++ lib/src/HomeState.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pixplace/pages.dart';
 
+import 'package:pixplace/widgets/DataSearch.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
