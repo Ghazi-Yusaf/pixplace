1b064e87ee82b8a67afbc48b9950517d7680a1b2
xecarlox94 <jf94.uk@gmail.com>
2021-02-17 21:15:29 +0000

    creating form for image

--- lib/src/camera/EditPictureScreen.dart
+++ lib/src/camera/EditPictureScreen.dart
@@ -2,16 +2,31 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 
-class EditPictureScreen extends StatelessWidget {
+
+
+class EditPictureScreen extends StatefulWidget {
   final String imagePath;
 
   const EditPictureScreen({Key key, this.imagePath}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => EditPictureScreenState(this.imagePath);
+}
+
+
+
+class EditPictureScreenState extends State<EditPictureScreen> {
+  final String imagePath;
+
+  EditPictureScreenState(this.imagePath);
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Display the Picture"),),
-      body: Image.file(File(imagePath)),
+      body: Image.file(File(this.imagePath)),
     );
   }
+
 }
