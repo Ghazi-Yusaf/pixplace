7972c021277610de7295fc9853b5c2b1ef8e8dcb
Ghazi Yusaf <ghaziyusaf@gmail.com>
2021-02-18 20:38:19 +0000

    Fix HomState.dart
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

--- lib/src/HomeState.dart
+++ lib/src/HomeState.dart
@@ -1,9 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:pixplace/pages.dart';
-
-import 'package:pixplace/widgets/DataSearch.dart';
+import 'package:pixplace/src/Feed.dart';
+import 'package:pixplace/src/ChallengePage.dart';
+import 'package:pixplace/src/LeaderBoardPage.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
+
+class _HomeState extends State<Home>{
+
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Feed(),
+    ProfilePage(),
+    Feed(),
+    ChallengePage(),
+    LeaderBoardPage(),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.pink,
+            height: 1,
+          ),
+            preferredSize: Size.fromHeight(1)
+        ),
+        centerTitle: true,
+        leading: PopupMenuButton<String>(
+          icon: Icon(Icons.menu, color: Colors.black,),
+          itemBuilder: (BuildContext context){
+            return MenuOptions.options.map((String option){
+              return PopupMenuItem<String>(
+                value: option,
+                child: Text(option),
+              );
+            }).toList();
+          },
+        ),
+        title: Text("PixPlace",style:TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white, elevation: 0,),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: "Home",
+                backgroundColor: Colors.pink
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined),
+              label: "Profile",
+                backgroundColor: Colors.pink
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera_alt_outlined),
+              label: "Post",
+                backgroundColor: Colors.pink
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_esports_outlined),
+              label: "Achievements",
+                backgroundColor: Colors.pink
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard_outlined),
+              label: "Leaderboards",
+                backgroundColor: Colors.pink
+            ),
+          ],
+          onTap: (index) {
+            setState((){
+              _currentIndex = index;
+            });
+          }
+      ),
+    );
+  }
+}
+
