decb40f3396decfc3a0017741abd8d9c476ef0e5
xecarlox94 <jf94.uk@gmail.com>
2021-03-02 21:40:28 +0000

    refactoring post card widget

--- lib/entities/Comment.dart
+++ lib/entities/Comment.dart
@@ -1,29 +1,18 @@
 class Comment {
-
   final String userId;
   final String text;
   final int likes;
 
-  Comment({
-    this.userId,
-    this.text,
-    this.likes
-  });
+  Comment({this.userId, this.text, this.likes});
 
   factory Comment.fromJson(Map<String, dynamic> element) {
     return Comment(
-      userId: element['userId'],
-      text: element['text'],
-      likes: element['likes']
-    );
+        userId: element['userId'],
+        text: element['text'],
+        likes: element['likes']);
   }
 
   Map<String, dynamic> toMap() {
-    return {
-      'userId': userId,
-      'text': text,
-      'likes': likes
-    };
+    return {'userId': userId, 'text': text, 'likes': likes};
   }
-
 }
--- lib/widgets/PostCardWidget.dart
+++ lib/widgets/PostCardWidget.dart
@@ -1,78 +1,91 @@
 import 'package:flutter/material.dart';
 
-Widget getImage(BuildContext context) => Image.network("https://picsum.photos/250/150",
-    fit: BoxFit.fitWidth,
-    width: MediaQuery.of(context).size.width
-  // loadingBuilder: (context, child, progress) => {
-  //     return progress == null ? child : LinearProgressIndicator()
-  // },
-);
+import 'package:pixplace/entities/Comment.dart';
+import 'package:pixplace/entities/Post.dart';
 
-Widget postDetails() => Row(
-  children: [
-    TextButton(onPressed: () => {}, child: Text('First Last')),
-    Spacer(),
-    IconButton(icon: Icon(Icons.expand_more_outlined), onPressed: () => {})
-  ],
-);
-
-Widget actionsBar() => Row(
-  children: [
-    IconButton(icon: Icon(Icons.star_border), onPressed: () => {}),
-    IconButton(icon: Icon(Icons.message_outlined), onPressed: () => {}),
-    IconButton(icon: Icon(Icons.send_sharp), onPressed: () => {}),
-    Spacer(),
-    IconButton(icon: Icon(Icons.collections_bookmark_outlined), onPressed: () => {}),
-  ],
-);
-
-class Message {
-  final username;
-  final message;
-  const Message(this.username, this.message);
-}
-
-List<Message> comments = [
-  new Message("Joe", "Really interesting"),
-  new Message("Sarah", "I wish I was there"),
-  new Message("Rebecca", "Thats a great picture")
-];
-
-Widget commentField(String username, String message) => ListTile(
-  leading: Text(username),
-  title: Text(message),
-  trailing: Icon(Icons.favorite_outline),
-);
-
-Widget commentsSection() => ExpansionTile(
-    title: Text("3 People commented ", style: TextStyle(color: Colors.pink),),
-    trailing: Text(""),
-    children: comments.map((comment) => commentField(comment.username, comment.message)).toList()
-);
-
-// Text("Comments Section");
-
-Widget cardContent(BuildContext context) => Container(
-  child: Column(
-    children: [
-      postDetails(),
-      getImage(context),
-      actionsBar(),
-      commentsSection(),
-    ],
-  ),
-);
-
-class PostCardWidget extends StatelessWidget {
-
-
+class PostCardWidget extends StatefulWidget {
   PostCardWidget({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: cardContent(context),
-    );
+  State<StatefulWidget> createState() {
+    return _PostCardState();
   }
 }
 
+class _PostCardState extends State<PostCardWidget> {
+  Post post = Post(
+      postId: "postID",
+      userId: "userID",
+      imageURL: "url",
+      caption: "caption",
+      tagId: "tagID",
+      commentIds: [],
+      likes: 5);
+
+  List<Comment> comments = [
+    new Comment(userId: "id", text: "Really interesting", likes: 3),
+    new Comment(userId: "id", text: "I wish I was there", likes: 5),
+    new Comment(userId: "id", text: "Thats a great picture", likes: 8)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    Widget getImage(BuildContext context) =>
+        Image.network("https://picsum.photos/250/150",
+            fit: BoxFit.fitWidth, width: MediaQuery.of(context).size.width
+            // loadingBuilder: (context, child, progress) => {
+            //     return progress == null ? child : LinearProgressIndicator()
+            // },
+            );
+
+    Widget postDetails() => Row(
+          children: [
+            TextButton(onPressed: () => {}, child: Text('First Last')),
+            Spacer(),
+            IconButton(
+                icon: Icon(Icons.expand_more_outlined), onPressed: () => {})
+          ],
+        );
+
+    Widget actionsBar() => Row(
+          children: [
+            IconButton(icon: Icon(Icons.star_border), onPressed: () => {}),
+            IconButton(icon: Icon(Icons.message_outlined), onPressed: () => {}),
+            IconButton(icon: Icon(Icons.send_sharp), onPressed: () => {}),
+            Spacer(),
+            IconButton(
+                icon: Icon(Icons.collections_bookmark_outlined),
+                onPressed: () => {}),
+          ],
+        );
+
+    Widget commentField(String username, String message) => ListTile(
+          leading: Text(username),
+          title: Text(message),
+          trailing: Icon(Icons.favorite_outline),
+        );
+
+    Widget commentsSection() => ExpansionTile(
+        title: Text(
+          "3 People commented ",
+          style: TextStyle(color: Colors.pink),
+        ),
+        trailing: Text(""),
+        children: this
+            .comments
+            .map((comment) => commentField("username", comment.text))
+            .toList());
+
+    return Card(
+        child: Container(
+      child: Column(
+        children: [
+          postDetails(),
+          getImage(context),
+          actionsBar(),
+          commentsSection(),
+        ],
+      ),
+    ));
+  }
+}
