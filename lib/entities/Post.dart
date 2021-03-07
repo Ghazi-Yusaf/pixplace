50918c09973128c927a5552145447c6bd18b6746
xecarlox94 <jf94.uk@gmail.com>
2021-03-07 23:37:33 +0000

    changing the post's date field type

--- lib/entities/Post.dart
+++ lib/entities/Post.dart
@@ -1,48 +1,45 @@
 class Post {
-
   final String postId;
   final String userId;
   final String imageURL;
-  final String date;
+  final int date;
   final String location;
   final String caption;
   final String tagId;
   final List<String> commentIds;
   final List<String> stars;
 
-  Post({
-    this.postId,
-    this.userId,
-    this.imageURL,
-    this.date,
-    this.location,
-    this.caption,
-    this.tagId,
-    this.commentIds,
-    this.stars
-  });
+  Post(
+      {this.postId,
+      this.userId,
+      this.imageURL,
+      this.date,
+      this.location,
+      this.caption,
+      this.tagId,
+      this.commentIds,
+      this.stars});
 
-  Post.fromJson(Map<String, dynamic> json) :
-    postId = json['postId'],
-    userId = json['userId'],
-    date = json['date'],
-    imageURL = json['imageURL'],
-    location = json['location'],
-    caption = json['caption'],
-    tagId = json['tagId'],
-    commentIds = List.from(json['commentIds']),
-    stars = List.from(json['stars']);
+  Post.fromJson(Map<String, dynamic> json)
+      : postId = json['postId'],
+        userId = json['userId'],
+        date = json['date'],
+        imageURL = json['imageURL'],
+        location = json['location'],
+        caption = json['caption'],
+        tagId = json['tagId'],
+        commentIds = List.from(json['commentIds']),
+        stars = List.from(json['stars']);
 
-  Map<String, dynamic> toJson() =>
-    {
-      'postId': postId,
-      'userId': userId,
-      'imageURL': imageURL,
-      'date': date,
-      'location': location,
-      'caption': caption,
-      'tagId': tagId,
-      'commentIds': commentIds,
-      'stars': stars
-    };
+  Map<String, dynamic> toJson() => {
+        'postId': postId,
+        'userId': userId,
+        'imageURL': imageURL,
+        'date': date,
+        'location': location,
+        'caption': caption,
+        'tagId': tagId,
+        'commentIds': commentIds,
+        'stars': stars
+      };
 }
--- lib/src/Feed.dart
+++ lib/src/Feed.dart
@@ -21,26 +21,28 @@ class _FeedState extends State<Feed> {
           child: Text('Add'),
           onPressed: () {
             String id = Uuid().v1();
-            Firestore.setDocument('Posts', id, Post(
-              postId: id,
-              userId: 'Mr Test User is cool',
-              imageURL: 'https://miro.medium.com/max/780/1*SIYbAut8gL4cAqMgk19-6Q.png',
-              date: DateFormat.yMMMEd().format(DateTime.now()),
-              location: 'Scotland',
-              caption: 'This is a beautiful picture of the sunset from the clouds that everyone can enjoy muchly.',
-              commentIds: [],
-              stars: []
-            ).toJson());
+            Firestore.setDocument(
+                'Posts',
+                id,
+                Post(
+                    postId: id,
+                    userId: 'Mr Test User is cool',
+                    imageURL:
+                        'https://miro.medium.com/max/780/1*SIYbAut8gL4cAqMgk19-6Q.png',
+                    date: DateTime.now().millisecondsSinceEpoch,
+                    location: 'Scotland',
+                    caption:
+                        'This is a beautiful picture of the sunset from the clouds that everyone can enjoy muchly.',
+                    commentIds: [],
+                    stars: []).toJson());
           },
         ),
         TextButton(
-          child: Text('Delete'),
-          onPressed: () => {
-            Firestore.firestore.collection('Posts').get().then((value) => {
-              Firestore.removeDocument('Posts', value.docs[0].id)
-            })
-          }
-        )
+            child: Text('Delete'),
+            onPressed: () => {
+                  Firestore.firestore.collection('Posts').get().then((value) =>
+                      {Firestore.removeDocument('Posts', value.docs[0].id)})
+                })
       ],
       body: StreamBuilder(
         stream: Firestore.getDocuments('Posts'),
@@ -50,22 +52,19 @@ class _FeedState extends State<Feed> {
               itemCount: snapshot.data.docs.length,
               itemBuilder: (content, index) {
                 return PostWidget(
-                  post: Post.fromJson(snapshot.data.docs.reversed.toList()[index].data()),
+                  post: Post.fromJson(
+                      snapshot.data.docs.reversed.toList()[index].data()),
                 );
               },
             );
-          }
-          else if (snapshot.hasError) {
+          } else if (snapshot.hasError) {
             return Text('An error has occured, please try again later.');
-          }
-          else {
+          } else {
             return Padding(
               padding: const EdgeInsets.all(20.0),
               child: Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.pink)
-                )
-              ),
+                  child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.pink))),
             );
           }
         },
