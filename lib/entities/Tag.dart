65b070e5c647abe16c00a3e1242c61807dd9a74f
xecarlox94 <jf94.uk@gmail.com>
2021-03-13 11:45:36 +0000

    adding challenge fields

--- lib/entities/Tag.dart
+++ lib/entities/Tag.dart
@@ -1,13 +1,10 @@
 class Tag {
   final String tagID;
+  final String name;
 
-  Tag({
-    this.tagID,
-  });
+  Tag({this.tagID, this.name});
 
   Tag.fromJson(Map<String, dynamic> json) : tagID = json['tagID'];
 
-  Map<String, dynamic> toJson() => {
-        'tagID': tagID,
-      };
+  Map<String, dynamic> toJson() => {'tagID': tagID, 'name': name};
 }
