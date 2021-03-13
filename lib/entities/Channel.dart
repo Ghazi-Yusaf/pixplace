c4f751c1c321f29ca1d6999c45e168f2fc924b84
xecarlox94 <jf94.uk@gmail.com>
2021-03-13 14:21:21 +0000

    adding challenge and tag entities to backend

--- lib/entities/Channel.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pixplace/entities/Tag.dart';
-
-class Channel extends Tag {
-  final String owner;
-
-  Channel(String tagID, this.owner) : super(tagID);
-}
--- lib/entities/Tag.dart
+++ lib/entities/Tag.dart
@@ -1,10 +1,15 @@
 class Tag {
   final String tagID;
   final String name;
+  final String ownerID;
 
-  Tag({this.tagID, this.name});
+  Tag({this.tagID, this.name, this.ownerID});
 
-  Tag.fromJson(Map<String, dynamic> json) : tagID = json['tagID'];
+  Tag.fromJson(Map<String, dynamic> json)
+      : tagID = json['tagID'],
+        name = json['name'],
+        ownerID = json['ownerID'];
 
-  Map<String, dynamic> toJson() => {'tagID': tagID, 'name': name};
+  Map<String, dynamic> toJson() =>
+      {'tagID': tagID, 'name': name, 'ownerID': ownerID};
 }
