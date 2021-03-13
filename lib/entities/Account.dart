079cdd11c79ac86870fdea62c2317a7335422e65
xecarlox94 <jf94.uk@gmail.com>
2021-03-13 10:50:16 +0000

    removed user class code, merged it with account class

--- lib/entities/Account.dart
+++ lib/entities/Account.dart
@@ -1,28 +1,46 @@
 class Account {
-
   final String userId;
   final String biography;
   final List<String> friends;
   final int experience;
+  final String profileImageURL;
+  final String bioText;
+  final List<String> postsIDs;
+  final List<String> tagsIDs;
+  final List<String> collectionsIDs;
 
   Account({
     this.userId,
     this.biography,
     this.friends,
-    this.experience
+    this.experience,
+    this.profileImageURL,
+    this.bioText,
+    this.postsIDs,
+    this.tagsIDs,
+    this.collectionsIDs,
   });
 
-  Account.fromJson(Map<String, dynamic> element) :
-    userId = element['userId'],
-    biography = element['biography'],
-    friends = element['friends'],
-    experience = element['experience'];
+  Account.fromJson(Map<String, dynamic> element)
+      : userId = element['userId'],
+        biography = element['biography'],
+        friends = element['friends'],
+        experience = element['experience'],
+        profileImageURL = element['profileImageURL'],
+        bioText = element['bioText'],
+        postsIDs = List.from(element['postsIDs']),
+        tagsIDs = List.from(element['tagsIDs']),
+        collectionsIDs = List.from(element['collectionsIDs']);
 
-  Map<String, dynamic> toJson() =>
-    {
-      'userId': userId,
-      'biography': biography,
-      'friends': friends,
-      'experience': experience
-    };
+  Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'biography': biography,
+        'friends': friends,
+        'experience': experience,
+        'profileImageURL': profileImageURL,
+        'bioText': bioText,
+        'postsIDs': postsIDs,
+        'tagsIDs': tagsIDs,
+        'collectionsIDs': collectionsIDs
+      };
 }
