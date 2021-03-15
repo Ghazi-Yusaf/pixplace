73dd8e2df7307454f24fb3ed19b7183ec4b04264
xecarlox94 <jf94.uk@gmail.com>
2021-03-15 18:45:32 +0000

    fixing bugs

--- lib/entities/Comment.dart
+++ lib/entities/Comment.dart
@@ -5,14 +5,20 @@ class Comment {
   final String text;
   final int likes;
 
-  Comment({this.commentId, this.userId, this.date, this.text});
+  Comment({this.commentId, this.userId, this.date, this.text, this.likes});
 
   Comment.fromJson(Map<String, dynamic> json)
       : commentId = json['commentId'],
         userId = json['userId'],
         date = json['date'],
-        text = json['text'];
+        text = json['text'],
+        likes = json['likes'];
 
-  Map<String, dynamic> toJson() =>
-      {'commentId': commentId, 'userId': userId, 'date': date, 'text': text};
+  Map<String, dynamic> toJson() => {
+        'commentId': commentId,
+        'userId': userId,
+        'date': date,
+        'text': text,
+        'likes': likes
+      };
 }
