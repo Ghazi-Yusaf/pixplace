60e7c7d471fcdb26815e3900e73fc2550c51a74f
xecarlox94 <jf94.uk@gmail.com>
2021-03-21 17:37:05 +0000

    trying blur face

--- lib/firebase/services/blurrFace.dart
+++ lib/firebase/services/blurrFace.dart
@@ -1,9 +1,10 @@
-import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
-import 'package:image/image.dart';
 
-void blurFacePictures(File file) async {
+Future<File> blurFacePictures(String filePath) async {
+  File file = new File(filePath);
+
   String url = "176.58.121.80:5000";
 
   var request = http.MultipartRequest(
@@ -11,57 +12,28 @@ void blurFacePictures(File file) async {
     Uri.http(url, "/"),
   );
 
-  request.headers["Content-Type"]='multipart/form-data';
+  request.headers["Content-Type"] = 'multipart/form-data';
 
   request.files.add(
     http.MultipartFile.fromBytes(
       "image",
       file.readAsBytesSync(),
-    //   contentType: MediaType(
-    //       "image", "${image.path.split(".").last}"),
+      //   contentType: MediaType(
+      //       "image", "${image.path.split(".").last}"),
     ),
   );
 
   var response = await request.send();
 
+  List<List<int>> byteStream = await response.stream.toList();
 
-
-  await response.stream.toList().then((value) {
-    List<int> newbyteStream = value.expand((element) => element).toList();
-    print(newbyteStream);
-    Image image = decodeJpg(newbyteStream);
-
-    File('thumbnail.jpg')..writeAsBytesSync(encodePng(image));
-  });
-
-  // EXAMPLE
-  //https://pub.dev/packages/image
-
-  
-
-
-
-
-
-
-  // print(response.statusCode);
-  // print(response.toString());
-  //
-  // print(response.headers);
-  print(response.stream.toString());
+  List<int> newbyteStream = byteStream.expand((element) => element).toList();
+  print(newbyteStream.length);
 }
 
+void main() async {
+  String filePath =
+      Directory.current.path + "\\lib\\firebase\\services\\meme.png";
 
-void main() {
-  String filePath = Directory.current.path+"\\lib\\firebase\\services\\meme.png";
-
-  File file = new File(filePath);
-
-  blurFacePictures(file);
+  File file = await blurFacePictures(filePath);
 }
-
-
-
-
-
-
