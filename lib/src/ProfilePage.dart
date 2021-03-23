b73a6fb0549ceb495fbe856f5f7a5433c24dff25
xecarlox94 <jf94.uk@gmail.com>
2021-03-23 12:16:06 +0000

    fixing profile page

--- lib/src/ProfilePage.dart
+++ lib/src/ProfilePage.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
@@ -7,142 +8,149 @@ import 'package:pixplace/firebase/Firestore.dart';
 import 'package:pixplace/firebase/UserManager.dart';
 import 'dart:math';
 
-int getLevel() {
-  return pow(((5.0 / 4.0) * account.experience), 1.0 / 3.0).toInt();
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-double getPercentage() {
-  return (account.experience - ((4 * pow(getLevel(), 3)) / 5)) /
-      (((4 * pow(getLevel() + 1, 3)) / 5) - ((4 * pow(getLevel(), 3)) / 5));
-}
+class _ProfilePageState extends State<ProfilePage> {
+  Account account;
+  // List<Widget> photos;
 
-getPhotos() async {
-  account.postIDs.forEach((post) async {
-    String url = await Firestore.getDocument('Posts', post)
-        .then((document) => document.data()['imageURL']);
-    if (url != null) {
-      print("new URL" + url);
-    } else {
-      print("url is null");
-    }
-    photos.add(Image.network(url));
-  });
-}
+  int getLevel(Account account) {
+    return pow(((5.0 / 4.0) * account.experience), 1.0 / 3.0).toInt();
+  }
 
-Account account;
-List<Widget> photos;
+  double getPercentage(Account account) {
+    return (account.experience - ((4 * pow(getLevel(account), 3)) / 5)) /
+        (((4 * pow(getLevel(account) + 1, 3)) / 5) -
+            ((4 * pow(getLevel(account), 3)) / 5));
+  }
 
-Widget profileImage() => Padding(
-      padding: EdgeInsets.only(top: 30),
-      child: CircularPercentIndicator(
-        radius: 200,
-        backgroundWidth: 15,
-        lineWidth: 15,
-        animation: true,
-        progressColor: Colors.pink,
-        percent: getPercentage(),
-        center: Container(
-            width: 165,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/userAvatar.png')))),
-      ),
-    );
+  // getPhotos(Account account) async {
+  //   account.postIDs.forEach((post) async {
+  //     String url = await Firestore.getDocument('Posts', post)
+  //         .then((document) => document.data()['imageURL']);
+  //     if (url != null) {
+  //       print("new URL" + url);
+  //     } else {
+  //       print("url is null");
+  //     }
+  //     photos.add(Image.network(url));
+  //   });
+  // }
 
-Widget headerMenu() => Padding(
-      padding: EdgeInsets.only(bottom: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(
-              Icons.favorite,
-              color: Colors.pink,
-              size: 45.0,
+  Widget profileImage(Account account) => Padding(
+        padding: EdgeInsets.only(top: 30),
+        child: CircularPercentIndicator(
+          radius: 200,
+          backgroundWidth: 15,
+          lineWidth: 15,
+          animation: true,
+          progressColor: Colors.pink,
+          percent: getPercentage(account),
+          center: Container(
+              width: 165,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/userAvatar.png')))),
+        ),
+      );
+
+  Widget headerMenu() => Padding(
+        padding: EdgeInsets.only(bottom: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 45.0,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(
-              Icons.add_circle_rounded,
-              color: Colors.green,
-              size: 45.0,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(
+                Icons.add_circle_rounded,
+                color: Colors.green,
+                size: 45.0,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 45.0,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(
+                Icons.star,
+                color: Colors.amber,
+                size: 45.0,
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
 
-Widget nameAndXP() => Padding(
-      padding: EdgeInsets.symmetric(vertical: 25),
-      child: Text('${account.username} Level ${getLevel()}'),
-    );
+  Widget nameAndXP(Account account) => Padding(
+        padding: EdgeInsets.symmetric(vertical: 25),
+        child: Text('${account.username} Level ${getLevel(account)}'),
+      );
 
-Widget header() => Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [profileImage(), nameAndXP(), headerMenu()],
-    );
+  Widget header(Account account) => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [profileImage(account), nameAndXP(account), headerMenu()],
+      );
 
-Widget bio() => Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
-        child: Center(
-          child: Text(
-            account.bioText,
-            textAlign: TextAlign.center,
+  Widget bio(Account account) => Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+          child: Center(
+            child: Text(
+              account.bioText,
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
-      ),
-    );
+      );
 
-Widget line() => Container(
-      color: Colors.pink,
-      height: 2,
-    );
+  Widget line() => Container(
+        color: Colors.pink,
+        height: 2,
+      );
 
-List<Widget> profilePage = [
-  SliverList(
-    delegate: SliverChildListDelegate([
-      header(),
-      bio(),
-      line(),
-    ]),
-  ),
-  SliverGrid.count(
-    crossAxisCount: 3,
-    mainAxisSpacing: 5,
-    crossAxisSpacing: 5,
-    children: [photos.first],
-  ),
-];
+  Future<DocumentSnapshot> getUserDoc() async {
+    User user = await UserManager.getCurrentUser();
+    DocumentSnapshot userDoc =
+        await Firestore.getDocument('Accounts', user.uid);
+    return userDoc;
+  }
 
-class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Widget> profilePage(Account account) => [
+          SliverList(
+            delegate: SliverChildListDelegate([
+              header(account),
+              bio(account),
+              line(),
+            ]),
+          ),
+          SliverGrid.count(
+            crossAxisCount: 3,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            // children: [this.photos.first],
+          ),
+        ];
     return Scaffold(
-      body: FutureBuilder<String>(
-          future: UserManager.getCurrentUser().then((user) => user.uid),
-          builder: (context, user) {
-            return FutureBuilder<DocumentSnapshot>(
-                future: Firestore.getDocument('Accounts', user.data),
-                builder: (context, first) {
-                  account = Account.fromJson(first.data.data());
-                  getPhotos();
-                  return CustomScrollView(slivers: profilePage);
-                });
-          }),
-    );
+        body: FutureBuilder<DocumentSnapshot>(
+            future: getUserDoc(),
+            builder: (context, snapshot) {
+              account = Account.fromJson(snapshot.data.data());
+              // getPhotos(account);
+              return CustomScrollView(slivers: profilePage(account));
+            }));
   }
 }
